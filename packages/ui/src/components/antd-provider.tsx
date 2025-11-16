80e70e9d4cf5b3b4a051e552b0647e0f53424ae2
duongductrong <duongductrong06@gmail.com>
2025-11-16 14:46:04 +0700

    feat(ui): enhance AntdProvider to support CSS variables for color tokens and remove unused Button component

--- packages/ui/src/components/antd-provider.tsx
+++ packages/ui/src/components/antd-provider.tsx
@@ -2,10 +2,33 @@
 
 import { StyleProvider } from "@ant-design/cssinjs";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import { ConfigProvider } from "antd";
-import { PropsWithChildren } from "react";
+import { ConfigProvider, theme } from "antd";
+import { PropsWithChildren, useInsertionEffect } from "react";
 
-export const AntdProvider = ({ children }: PropsWithChildren) => {
+export const AntdProvider = ({
+  children,
+  allowTokensToCSSVariables = true,
+}: PropsWithChildren<{ allowTokensToCSSVariables?: boolean }>) => {
+  const { token: tokens } = theme.useToken();
+
+  useInsertionEffect(() => {
+    if (allowTokensToCSSVariables) {
+      const style = document.createElement("style");
+      const colorTokens = Object.entries(tokens).filter(([key]) =>
+        key.toLowerCase().includes("color")
+      );
+      
+      style.innerHTML = `
+      :root {
+        ${colorTokens
+          .map(([key, value]) => `--${key}: ${value.toString()};`)
+          .join("\n")}
+      }
+    `;
+      document.head.appendChild(style);
+    }
+  }, [tokens, allowTokensToCSSVariables]);
+
   return (
     <StyleProvider layer>
       <AntdRegistry>
